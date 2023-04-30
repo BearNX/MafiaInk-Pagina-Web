d4d662f032f939d96e3a392e69f8ada148c01634
BearNX <113077865+BearNX@users.noreply.github.com>
2023-04-30 02:46:08 +0000

    Añadiendo base de datos

--- conexion.php
+++ conexion.php
@@ -1,21 +1,29 @@
-// Crea una conexión a la base de datos
-$conn = mysqli_connect("localhost", "usuario", "contraseña", "basedatos");
+<?php
+$servername = "localhost"; // Reemplaza "localhost" con el nombre de tu servidor
+$username = "root"; // Reemplaza "tu_usuario" con tu usuario de MySQL
+$password = "programacionweb2022#"; // Reemplaza "tu_contraseña" con tu contraseña de MySQL
+$dbname = "projectink"; // Reemplaza "tu_base_de_datos" con el nombre de tu base de datos
 
-// Prepara una consulta SQL
-$stmt = mysqli_prepare($conn, "SELECT nombre, email FROM usuarios WHERE iduser = ?");
+// Crea la conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Asigna el valor del parámetro a la consulta SQL
-$id = 1;
-mysqli_stmt_bind_param($stmt, "i", $id);
+// Verifica la conexión
+if ($conn->connect_error) {
+  die("Conexión fallida: " . $conn->connect_error);
+}
+echo "Conexión exitosa";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $email = $_POST["email_reg"];
+  $password = $_POST["psw_reg"];
 
-// Ejecuta la consulta SQL
-mysqli_stmt_execute($stmt);
+  $sql = "INSERT INTO usuarios (email, password) VALUES ('$email', '$password')";
 
-// Procesa los resultados
-mysqli_stmt_bind_result($stmt, $nombre, $email);
-while (mysqli_stmt_fetch($stmt)) {
-    echo $nombre . " - " . $email . "<br>";
+  if ($conn->query($sql) === TRUE) {
+    echo "Registro exitoso";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 }
 
-// Cierra la conexión a la base de datos
-mysqli_close($conn);
+$conn->close();
+?>
